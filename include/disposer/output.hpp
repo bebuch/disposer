e3e184f769491623e1485ca45eaf852224fa8ee7
Benjamin Roland Buch <benni.buch@gmail.com>
2015-08-04 11:18:39 +0200

    put not as template, if only one type in output (version is broken\!\!\!)

--- include/disposer/output.hpp
+++ include/disposer/output.hpp
@@ -53,118 +53,146 @@ namespace disposer{
 
 	using output_list = std::unordered_map< std::string, impl::output::output_entry >;
 
-	template < typename T >
-	class module_output_interface{
-	public:
-		using value_type = T;
+	template < typename ... Outputs >
+	output_list make_output_list(Outputs& ... outputs);
 
 
-		module_output_interface(impl::output::signal_t& signal, type_index const& type): signal(signal), type(type) {}
+	namespace impl{ namespace output{
 
 
-		void operator()(std::size_t id, value_type&& value){
-			trigger_signal(id, std::make_shared< value_type >(std::move(value)));
-		}
-
-		void operator()(std::size_t id, value_type const& value){
-			trigger_signal(id, std::make_shared< value_type >(value));
-		}
-
-		void operator()(std::size_t id, std::shared_ptr< value_type >&& value){
-			trigger_signal(id, std::move(value));
-		}
-
-		void operator()(std::size_t id, std::shared_ptr< value_type > const& value){
-			trigger_signal(id, value);
-		}
+		template < typename T >
+		class module_output_interface{
+		public:
+			using value_type = T;
 
 
-	private:
-		impl::output::signal_t& signal;
-		type_index const& type;
-
-		void trigger_signal(std::size_t id, std::shared_ptr< value_type > const& value){
-			signal(id, reinterpret_cast< impl::input::any_type const& >(value), type);
-		}
-	};
+			module_output_interface(signal_t& signal, type_index const& type): signal(signal), type(type) {}
 
 
-	template < typename T >
-	class module_output_interface< std::future< T > >{
-	public:
-		using value_type = std::future< T >;
+			void operator()(std::size_t id, value_type&& value){
+				trigger_signal(id, std::make_shared< value_type >(std::move(value)));
+			}
+
+			void operator()(std::size_t id, value_type const& value){
+				trigger_signal(id, std::make_shared< value_type >(value));
+			}
+
+			void operator()(std::size_t id, std::shared_ptr< value_type >&& value){
+				trigger_signal(id, std::move(value));
+			}
+
+			void operator()(std::size_t id, std::shared_ptr< value_type > const& value){
+				trigger_signal(id, value);
+			}
 
 
-		module_output_interface(impl::output::signal_t& signal, type_index const& type): signal(signal), type(type) {}
+		private:
+			signal_t& signal;
+			type_index const& type;
+
+			void trigger_signal(std::size_t id, std::shared_ptr< value_type > const& value){
+				signal(id, reinterpret_cast< impl::input::any_type const& >(value), type);
+			}
+		};
 
 
-		void operator()(std::size_t id, value_type&& value){
-			auto val = std::make_shared< impl::input::future_type< T > >(std::move(value));
-			signal(id, reinterpret_cast< impl::input::any_type const& >(val));
-		}
+		template < typename T >
+		class module_output_interface< std::future< T > >{
+		public:
+			using value_type = std::future< T >;
 
 
-	private:
-		type_index const& type;
-		impl::output::signal_t& signal;
-	};
+			module_output_interface(signal_t& signal, type_index const& type): signal(signal), type(type) {}
+
+
+			void operator()(std::size_t id, value_type&& value){
+				auto val = std::make_shared< impl::input::future_type< T > >(std::move(value));
+				signal(id, reinterpret_cast< impl::input::any_type const& >(val), type);
+			}
+
+
+		private:
+			signal_t& signal;
+			type_index const& type;
+		};
+
+
+		template < typename T, typename ... U >
+		class module_output{
+		public:
+			static constexpr auto value_types = hana::tuple_t< T, U ... >;
+
+			static_assert(
+				!hana::fold(hana::transform(value_types, hana::traits::is_const), false, std::logical_or<>()),
+				"module_output types are not allowed to be const"
+			);
+
+			static_assert(
+				!hana::fold(hana::transform(value_types, hana::traits::is_reference), false, std::logical_or<>()),
+				"module_output types are not allowed to be references"
+			);
+
+	// 		static_assert(
+	// 			hana::unique(hana::tuple_t< T, U ... >),
+	// 			"module_output must have distict types"
+	// 		);
+
+
+			module_output(std::string const& name): name(name) {}
+
+			module_output(module_output const&) = delete;
+			module_output(module_output&&) = delete;
+
+			module_output& operator=(module_output const&) = delete;
+			module_output& operator=(module_output&&) = delete;
+
+
+			std::string const name;
+
+
+			template < typename V, typename W >
+			auto put(std::size_t id, W&& value){
+				static_assert(hana::contains(value_types, hana::type< V >), "type V in put< V > is not a module_output type");
+
+				if(type != type_id_with_cvr< V >()){
+					throw std::logic_error(
+						"module_output '" + name + "' put type '" +
+						type_id_with_cvr< V >().pretty_name() +
+						"', but active type is '" + type.pretty_name() + "'"
+					);
+				}
+
+				return module_output_interface< V >(signal, type)(id, static_cast< W&& >(value));
+			}
+
+
+		private:
+			signal_t signal;
+			type_index type;
+
+			template < typename ... Outputs >
+			friend output_list disposer::make_output_list(Outputs& ... outputs);
+		};
+
+
+	} }
 
 
 	template < typename T, typename ... U >
-	class module_output{
+	class module_output: public impl::output::module_output< T, U ... >{
 	public:
-		static constexpr auto value_types = hana::tuple_t< T, U ... >;
+		using impl::output::module_output< T, U ... >::module_output;
+	};
 
-		static_assert(
-			!hana::fold(hana::transform(value_types, hana::traits::is_const), false, std::logical_or<>()),
-			"module_output types are not allowed to be const"
-		);
+	template < typename T >
+	class module_output< T >: public impl::output::module_output< T >{
+	public:
+		using impl::output::module_output< T >::module_output;
 
-		static_assert(
-			!hana::fold(hana::transform(value_types, hana::traits::is_reference), false, std::logical_or<>()),
-			"module_output types are not allowed to be references"
-		);
-
-// 		static_assert(
-// 			hana::unique(hana::tuple_t< T, U ... >),
-// 			"module_output must have distict types"
-// 		);
-
-
-		module_output(std::string const& name): name(name) {}
-
-		module_output(module_output const&) = delete;
-		module_output(module_output&&) = delete;
-
-		module_output& operator=(module_output const&) = delete;
-		module_output& operator=(module_output&&) = delete;
-
-
-		std::string const name;
-
-
-		template < typename V, typename W >
+		template < typename W >
 		auto put(std::size_t id, W&& value){
-			static_assert(hana::contains(value_types, hana::type< V >), "type V in put< V > is not a module_output type");
-
-			if(type != type_id_with_cvr< V >()){
-				throw std::logic_error(
-					"module_output '" + name + "' put type '" +
-					type_id_with_cvr< V >().pretty_name() +
-					"', but active type is '" + type.pretty_name() + "'"
-				);
-			}
-
-			return module_output_interface< V >(signal, type)(id, static_cast< W&& >(value));
+			impl::output::module_output< T >::template put< T >(id, static_cast< W&& >(value));
 		}
-
-
-	private:
-		impl::output::signal_t signal;
-		type_index type;
-
-		template < typename ... Outputs >
-		friend output_list make_output_list(Outputs& ... outputs);
 	};
 
 
