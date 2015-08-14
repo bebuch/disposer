f055432b89ba84561c6ba1b9703d1c05f21858ca
Benjamin Roland Buch <benni.buch@gmail.com>
2015-08-14 22:52:34 +0200

    move type_position in a separate file

--- include/disposer/output.hpp
+++ include/disposer/output.hpp
@@ -14,6 +14,7 @@
 #include "output_base.hpp"
 #include "output_data.hpp"
 #include "is_type_unique.hpp"
+#include "type_position.hpp"
 
 #include <boost/hana.hpp>
 #include <boost/hana/ext/std/type_traits.hpp>
@@ -61,31 +62,6 @@ namespace disposer{
 
 
 	namespace impl{ namespace output{
-
-
-		template < typename Ref, std::size_t Index, typename ... Tests >
-		struct type_position;
-
-		template < std::size_t Index, bool valid >
-		struct type_position_test: std::integral_constant< std::size_t, Index >{};
-
-		template < std::size_t Index >
-		struct type_position_test< Index, false >{
-			static_assert(Index == -1, "type_position did not find the type");
-		};
-
-		template < typename Ref, std::size_t Index, typename Test >
-		struct type_position< Ref, Index, Test >: type_position_test< Index, std::is_same< Ref, Test >::value >{};
-
-		template < typename Ref, std::size_t Index, typename Test, typename ... Tests >
-		struct type_position< Ref, Index, Test, Tests ... >: std::conditional_t<
-			std::is_same< Ref, Test >::value,
-			std::integral_constant< std::size_t, Index >,
-			type_position< Ref, 1 + Index, Tests ... >
-		>{};
-
-		template < typename Ref, typename ... Tests >
-		constexpr std::size_t type_position_v = type_position< Ref, 0, Tests ... >::value;
 
 
 		template < typename T, typename ... U >
---
+++ include/disposer/type_position.hpp
@@ -0,0 +1,46 @@
+//-----------------------------------------------------------------------------
+// Copyright (c) 2015 Benjamin Buch
+//
+// https://github.com/bebuch/disposer
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
+//-----------------------------------------------------------------------------
+#ifndef _disposer_type_position_hpp_INCLUDED_
+#define _disposer_type_position_hpp_INCLUDED_
+
+#include <type_traits>
+
+
+namespace disposer{
+
+
+	template < typename Ref, std::size_t Index, typename ... Tests >
+	struct type_position;
+
+	template < std::size_t Index, bool valid >
+	struct type_position_test: std::integral_constant< std::size_t, Index >{};
+
+	template < std::size_t Index >
+	struct type_position_test< Index, false >{
+		static_assert(Index == -1, "type_position did not find the type");
+	};
+
+	template < typename Ref, std::size_t Index, typename Test >
+	struct type_position< Ref, Index, Test >: type_position_test< Index, std::is_same< Ref, Test >::value >{};
+
+	template < typename Ref, std::size_t Index, typename Test, typename ... Tests >
+	struct type_position< Ref, Index, Test, Tests ... >: std::conditional_t<
+		std::is_same< Ref, Test >::value,
+		std::integral_constant< std::size_t, Index >,
+		type_position< Ref, 1 + Index, Tests ... >
+	>{};
+
+	template < typename Ref, typename ... Tests >
+	constexpr std::size_t type_position_v = type_position< Ref, 0, Tests ... >::value;
+
+
+}
+
+
+#endif
