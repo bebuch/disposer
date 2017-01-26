e1e04d7333d58658c73e75670d4916991a84fef9
Benjamin Roland Buch <benni.buch@gmail.com>
2017-01-26 17:06:30 +0100

    add a main page documention

---
+++ src/documention.cpp
@@ -0,0 +1,11 @@
+/// \mainpage Introduction
+///
+/// See https://github.com/bebuch/disposer
+///
+/// Or take a look into README.md which holds the introduction as Markdown
+/// source.
+///
+/// The following is the README.md (Github-Markdown) parsed by doxygen:
+///
+/// \includedoc README.md
+///
