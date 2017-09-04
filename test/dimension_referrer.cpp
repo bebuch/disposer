01f1936fb2ba518893c664b73ef0f80f255debc3
Benjamin Roland Buch <benni.buch@gmail.com>
2017-09-04 13:51:37 +0200

    more unit tests

--- test/dimension_referrer.cpp
+++ test/dimension_referrer.cpp
@@ -9,7 +9,8 @@ int main(){
 
 	auto list = dimension_list{
 			dimension_c< double, char, float >,
-			dimension_c< int, bool >
+			dimension_c< int, bool >,
+			dimension_c< short, unsigned, long, long long >
 		};
 	using list_t = decltype(list);
 
@@ -108,5 +109,45 @@ int main(){
 		auto type_5 = wrap.convert< list_t >
 			.value_type_of(hana::tuple_c< std::size_t, 2, 1 >);
 		static_assert(type_5 == hana::type_c< morph< bool, float, bool > >);
+	}
+
+	{
+		auto wrap = wrapped_type_ref_c< morph, 2, 0 >;
+		auto type_0 = wrap.convert< list_t >
+			.value_type_of(hana::tuple_c< std::size_t, 0, 0 >);
+		static_assert(type_0 == hana::type_c< morph< short, double > >);
+		auto type_1 = wrap.convert< list_t >
+			.value_type_of(hana::tuple_c< std::size_t, 0, 1 >);
+		static_assert(type_1 == hana::type_c< morph< unsigned, double > >);
+		auto type_2 = wrap.convert< list_t >
+			.value_type_of(hana::tuple_c< std::size_t, 0, 2 >);
+		static_assert(type_2 == hana::type_c< morph< long, double > >);
+		auto type_3 = wrap.convert< list_t >
+			.value_type_of(hana::tuple_c< std::size_t, 0, 3 >);
+		static_assert(type_3 == hana::type_c< morph< long long, double > >);
+		auto type_4 = wrap.convert< list_t >
+			.value_type_of(hana::tuple_c< std::size_t, 1, 0 >);
+		static_assert(type_4 == hana::type_c< morph< short, char > >);
+		auto type_5 = wrap.convert< list_t >
+			.value_type_of(hana::tuple_c< std::size_t, 1, 1 >);
+		static_assert(type_5 == hana::type_c< morph< unsigned, char > >);
+		auto type_6 = wrap.convert< list_t >
+			.value_type_of(hana::tuple_c< std::size_t, 1, 2 >);
+		static_assert(type_6 == hana::type_c< morph< long, char > >);
+		auto type_7 = wrap.convert< list_t >
+			.value_type_of(hana::tuple_c< std::size_t, 1, 3 >);
+		static_assert(type_7 == hana::type_c< morph< long long, char > >);
+		auto type_8 = wrap.convert< list_t >
+			.value_type_of(hana::tuple_c< std::size_t, 2, 0 >);
+		static_assert(type_8 == hana::type_c< morph< short, float > >);
+		auto type_9 = wrap.convert< list_t >
+			.value_type_of(hana::tuple_c< std::size_t, 2, 1 >);
+		static_assert(type_9 == hana::type_c< morph< unsigned, float > >);
+		auto type_10 = wrap.convert< list_t >
+			.value_type_of(hana::tuple_c< std::size_t, 2, 2 >);
+		static_assert(type_10 == hana::type_c< morph< long, float > >);
+		auto type_11 = wrap.convert< list_t >
+			.value_type_of(hana::tuple_c< std::size_t, 2, 3 >);
+		static_assert(type_11 == hana::type_c< morph< long long, float > >);
 	}
 }
