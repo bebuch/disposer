0ac259dc1dcab12e50e1863308feea8858019ab5
Benjamin Roland Buch <benni.buch@gmail.com>
2018-03-20 12:27:47 +0100

    reimplemented pretty_name

--- src/ct_pretty_name.cpp
+++ src/ct_pretty_name.cpp
@@ -16,19 +16,65 @@ namespace disposer{ namespace{
 			c == '_';
 	}
 
-	std::array< std::pair< std::string, std::string >, 12 > replace_table{{
-		{"long long", "long long"},
-		{"long double", "long double"},
-		{"float", "float32"},
-		{"double", "float64"},
-		{type_index::type_id< std::uint8_t >().pretty_name(), "uint8"},
-		{type_index::type_id< std::uint16_t >().pretty_name(), "uint16"},
-		{type_index::type_id< std::uint64_t >().pretty_name(), "uint64"},
-		{type_index::type_id< std::uint32_t >().pretty_name(), "uint32"},
-		{type_index::type_id< std::int8_t >().pretty_name(), "int8"},
-		{type_index::type_id< std::int16_t >().pretty_name(), "int16"},
-		{type_index::type_id< std::int64_t >().pretty_name(), "int64"},
-		{type_index::type_id< std::int32_t >().pretty_name(), "int32"}}};
+
+	namespace{
+
+		template < typename T >
+		std::string name(){
+			return type_index::type_id< T >().pretty_name();
+		}
+
+	}
+
+
+	auto const replace_table = []{
+			std::array< std::pair< std::string, std::string >, 17 > list{{
+					{name< char >(),               "char"},
+					{name< char16_t >(),           "char16_t"},
+					{name< char32_t >(),           "char32_t"},
+					{name< signed char >(),        "signed char"},
+					{name< unsigned char >(),      "unsigned char"},
+					{name< short >(),              "short"},
+					{name< unsigned short >(),     "unsigned short"},
+					{name< int >(),                "int"},
+					{name< unsigned int >(),       "unsigned int"},
+					{name< long >(),               "long"},
+					{name< unsigned long >(),      "unsigned long"},
+					{name< long long >(),          "long long"},
+					{name< unsigned long long >(), "unsigned long long"},
+					{name< float >(),              "float32"},
+					{name< double >(),             "float64"},
+					{name< long double >(),        "long double"},
+					{name< bool >(),               "bool"}
+				}};
+
+			auto exchange = [&list](
+					std::string const& first,
+					std::string&& second
+				){
+					for(auto& pair: list){
+						if(pair.first == first){
+							pair.second = std::move(second);
+						}
+					}
+				};
+
+			exchange(name< std::uint8_t >(), "uint8"),
+			exchange(name< std::uint16_t >(), "uint16"),
+			exchange(name< std::uint64_t >(), "uint64"),
+			exchange(name< std::uint32_t >(), "uint32"),
+			exchange(name< std::int8_t >(), "int8"),
+			exchange(name< std::int16_t >(), "int16"),
+			exchange(name< std::int64_t >(), "int64"),
+			exchange(name< std::int32_t >(), "int32");
+
+			std::sort(list.begin(), list.end(),
+				[](auto const& lhs, auto const& rhs){
+					return lhs.first.size() > rhs.first.size();
+				});
+
+			return list;
+		}();
 
 	inline std::optional< std::size_t > find_replace_index(
 		std::string_view source_text
@@ -77,27 +123,17 @@ namespace disposer{
 				continue;
 			}
 
+			test = false;
+
 			std::string_view rest(&type[i], type.size() - i);
 			auto const index = find_replace_index(rest);
 
 			if(!index){
 				result += c;
-				test = false;
 				continue;
 			}
 
-			auto const& replace_pair = replace_table[*index];
-			auto const& from = replace_pair.first;
-			auto const& to = replace_pair.second;
-
-			test = false;
-			if(type.size() - i > from.size()){
-				auto const ce = type[i + from.size()];
-				if(is_name_char(ce)){
-					result += c;
-					continue;
-				}
-			}
+			auto const& [from, to] = replace_table[*index];
 
 			result += to;
 			i += from.size() - 1;
